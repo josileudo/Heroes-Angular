2689af358f6f8a3c02fb3b155acb34726c4c905e
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-06-17 09:38:21 -0300

    create details from showing heroes

---
+++ src/app/components/hero-details/hero-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+import { Hero } from "../../hero"
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css']
+})
+
+export class HeroDetailsComponent  {
+
+  @Input() hero?:Hero
+
+}
+
