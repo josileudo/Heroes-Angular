cdf5d84a69e8de0b2bbec0c76476a91c5ed3d7c9
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-06-17 16:23:08 -0300

    adding observable in HeroService

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero'
 
 export const HEROES: Hero[] = [
-  {id:11, name: 'Loki'},
-  {id:12, name: 'Thor'},
-  {id:13, name: 'Iron-Man'},
-  {id:14, name: 'Batman'},
-  {id:15, name: 'Wolwerine'},
-  {id:16, name: 'Super-man'},
-  {id:17, name: 'Spider-man'},
-  {id:18, name: 'Tommy'},
-  {id:19, name: 'ScoobyDoo'},
-  {id:20, name: 'Fera'},
+  { id:11, name: 'Loki' },
+  { id:12, name: 'Thor' },
+  { id:13, name: 'Iron-Man' },
+  { id:14, name: 'Batman' },
+  { id:15, name: 'Wolwerine' },
+  { id:16, name: 'Super-man' },
+  { id:17, name: 'Spider-man' },
+  { id:18, name: 'Tommy' },
+  { id:19, name: 'ScoobyDoo' },
+  { id:20, name: 'Fera' },
 ]
